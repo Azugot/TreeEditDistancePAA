57ee875f0bd4ee6247b4ab66c28e9fe2ca979f7b
Azugot <augustoscardua@gmail.com>
2024-06-08 17:46:04 -0300

    Modified Readme; Modified gitignore Added LabelTree structure; Added Main.cpp Added Node structure

---
+++ LabelTree.hpp
@@ -0,0 +1,79 @@
+#include <String>
+#include "Node.hpp"
+#include <iostream>
+using namespace std;
+
+class LabelTree
+{
+private:
+    string name;
+    Node *root;
+
+public:
+    LabelTree(string name)
+    {
+        this->name = name;
+        this->root = new Node();
+    }
+    /**
+     * Adds a new node to the tree with the specified parent and child labels.
+     * If the parent node is not found, an error message is displayed and the function returns.
+     *
+     * @param parentLabel The label of the parent node.
+     * @param childLabel The label of the child node to be added.
+     */
+    void addNode(string parentLabel, string childLabel)
+    {
+        Node *parentNode = root->findNode(parentLabel);
+        if (parentNode == nullptr)
+        {
+            cout << "Parent node not found" << endl;
+            return;
+        }
+        Node *childNode = new Node(childLabel);
+        parentNode->addChild(childNode);
+    }
+
+    /**
+     * Removes a child node with the specified label from the parent node with the specified label.
+     * If either the parent node or the child node is not found, an error message is displayed.
+     *
+     * @param parentLabel The label of the parent node.
+     * @param childLabel The label of the child node to be removed.
+     */
+    void removeNode(string parentLabel, string childLabel)
+    {
+        Node *parentNode = root->findNode(parentLabel);
+        if (parentNode == nullptr)
+        {
+            cout << "Parent node not found" << endl;
+            return;
+        }
+        Node *childNode = parentNode->findNode(childLabel);
+        if (childNode == nullptr)
+        {
+            cout << "Child node not found" << endl;
+            return;
+        }
+        parentNode->removeNode(childNode);
+    }
+
+    /**
+     * Prints the labels of the tree nodes in a hierarchical structure.
+     * 
+     * @param node The root node of the tree to be printed.
+     * @param depth The current depth of the node in the tree (default is 0).
+     */
+    void printTree(Node *node, int depth = 0) const
+    {
+        if (node == nullptr)
+            return;
+        for (int i = 0; i < depth; ++i)
+            cout << "  ";
+        cout << node->getLabel() << endl;
+        for (auto child : node->getChildren())
+        {
+            printTree(child, depth + 1);
+        }
+    }
+};
---
+++ Node.hpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+private:
+    string label;
+    vector<Node *> children;
+
+public:
+    Node()
+    {
+        this->label = "";
+    }
+    Node(string label)
+    {
+        this->label = label;
+    }
+    void addChild(Node *child)
+    {
+        children.push_back(child);
+    }
+    void removeNode(Node *child)
+    {
+        for (int i = 0; i < children.size(); i++)
+        {
+            if (children[i] == child)
+            {
+                children.erase(children.begin() + i);
+                return;
+            }
+        }
+    }
+    Node *findNode(string label)
+    {
+        if (this->label == label)
+        {
+            return this;
+        }
+        for (Node *child : children)
+        {
+            Node *foundNode = child->findNode(label);
+            if (foundNode != nullptr)
+            {
+                return foundNode;
+            }
+        }
+        return nullptr;
+    }
+    string getLabel() const
+    {
+        return label;
+    }
+    vector<Node *> getChildren() const
+    {
+        return children;
+    }
+};
